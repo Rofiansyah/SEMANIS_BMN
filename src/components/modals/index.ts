5ab9ee626f084ebd446eb057639fd4938f3f6360
Rofiansyah <wizmanrofiansyah@gmail.com>
2025-08-12 14:10:38 +0700

    ubah admin

--- src/components/modals/index.ts
+++ src/components/modals/index.ts
@@ -1,7 +1,7 @@
 export { TambahBarangModal } from './TambahBarangModal';
 export { TambahMerekModal } from './TambahMerekModal';
 export { TambahLokasiModal } from './TambahLokasiModal';
-export { default as TambahKategoriModal } from './TambahKategoriModal';
+export { TambahKategoriModal } from './TambahKategoriModal';
 export { EditBarangModal } from './EditBarangModal';
 export { default as EditLokasiModal } from './EditLokasiModal';
 export { default as EditKategoriModal } from './EditKategoriModal';
