5cf642ae4615548ab62cd7fdfe5083f5f82152a1
Wizman Rofiansyah <72426836+Rofiansyah@users.noreply.github.com>
2025-08-20 10:05:18 +0700

    ubah admin

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -285,8 +285,8 @@ export default function DashboardUserPage() {
   {/* Search & Toggle Filter */}
   <div className="flex flex-col lg:flex-row gap-3 lg:gap-4 mb-4">
     {/* Search Box */}
-    <div className="relative max-w-md w-full">
-      <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-4 h-4" />
+    <div className="flex-1 relative">
+      <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-700 w-4 h-4" />
       <Input
         type="text"
         placeholder="Cari berdasarkan nama, deskripsi, atau kode barang..."
